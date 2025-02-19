f9c502df2780949f51c3e3374785971cbbcfe9e3
Walid Parast <walidparast1@gmail.com>
2025-02-19 15:13:17 -0500

    Fixed crashing issue, caused by CustomAdapter returning 5 as getCount instead of the new size after deletion.

--- app/src/main/java/edu/temple/namelist/MainActivity.kt
+++ app/src/main/java/edu/temple/namelist/MainActivity.kt
@@ -12,7 +12,7 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var names: List<String>
+    private lateinit var names: List<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
